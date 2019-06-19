3686d96662521f99819696ec045de0fc56f735d8
shizhenshuang <shizhenshuang@utry.cn>
2019-06-19 22:33:45 +0800

    UserInfo类中去掉lombok注解

--- src/main/java/utry/hikaricp/model/UserInfo.java
+++ src/main/java/utry/hikaricp/model/UserInfo.java
@@ -1,11 +1,31 @@
 package utry.hikaricp.model;
 
-import lombok.Data;
-
-@Data
 public class UserInfo {
     private Integer id;
 
     private String remarks;
 
+    public UserInfo() {
+    }
+
+    public UserInfo(Integer id, String remarks) {
+        this.id = id;
+        this.remarks = remarks;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
 }
